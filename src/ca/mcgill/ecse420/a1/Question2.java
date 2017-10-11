7400b03a026647144800243b91351602574b89a8
Alessandro-Commodari <commodari.a@gmail.com>
2017-10-10 21:43:12 -0400

    question 2

--- src/ca/mcgill/ecse420/a1/Question2.java
+++ src/ca/mcgill/ecse420/a1/Question2.java
@@ -2,44 +2,52 @@ package ca.mcgill.ecse420.a1;
 
 public class Question2 {
 
-  public static Object Lock1 = new Object();
-  public static Object Lock2 = new Object();
-  
   public static void main(String[] args) {
-     ThreadDemo1 T1 = new ThreadDemo1();
-     ThreadDemo2 T2 = new ThreadDemo2();
-     T1.start();
-     T2.start();
-  }
-  
-  private static class ThreadDemo1 extends Thread {
-     public void run() {
-        synchronized (Lock1) {
-           System.out.println("Thread 1: Holding lock 1...");
-           
-           try { Thread.sleep(10); }
-           catch (InterruptedException e) {}
-           System.out.println("Thread 1: Waiting for lock 2...");
-           
-           synchronized (Lock2) {
-              System.out.println("Thread 1: Holding lock 1 & 2...");
-           }
+    // the two locks in question
+    String l1 = "this is the first lock...";
+    String l2 = "this is the second lock...";
+
+    Thread thrd1 = new Thread() {
+      public void run() {
+
+        // first thread 1 tries to get the first lock which it does
+        synchronized (l1) {
+          System.out.println("Thread 1: Holding onto lock 1...");
+          try {
+            Thread.sleep(10);
+          } catch (InterruptedException e) {
+          }
+          System.out.println("Thread 1: Waiting for lock 2 to be free...");
+          // next thread 1 tries to get the second lock which in this case won't happen
+          synchronized (l2) {
+            System.out.println("Thread 1: Holding onto lock 1 and 2...");
+          }
         }
-     }
-  }
-  private static class ThreadDemo2 extends Thread {
-     public void run() {
-        synchronized (Lock2) {
-           System.out.println("Thread 2: Holding lock 2...");
-           
-           try { Thread.sleep(10); }
-           catch (InterruptedException e) {}
-           System.out.println("Thread 2: Waiting for lock 1...");
-           
-           synchronized (Lock1) {
-              System.out.println("Thread 2: Holding lock 1 & 2...");
-           }
+      }
+    };
+
+    Thread thrd2 = new Thread() {
+      public void run() {
+
+        // first thread 2 tries to get the second lock which it does
+        synchronized (l2) {
+          System.out.println("Thread 2: Holding onto lock 2...");
+
+          try {
+            Thread.sleep(10);
+          } catch (InterruptedException e) {
+          }
+          System.out.println("Thread 2: Waiting for lock 1 to be free...");
+          // next thread 1 tries to get the second lock which in this case won't happen
+          synchronized (l1) {
+            System.out.println("Thread 2: Holding onto lock 1 and 2...");
+          }
         }
-     }
+      }
+    };
+    // start both threads
+    thrd1.start();
+    thrd2.start();
+
   }
 }
