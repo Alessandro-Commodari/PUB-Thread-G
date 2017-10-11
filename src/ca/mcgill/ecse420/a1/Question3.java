b6495b0be2e9b72f5a6e71e7be845e34d04202e8
VittorioG <vittorio.mcgill@gmail.com>
2017-10-10 22:32:17 -0400

    Fixed Code. Took Deadlock pic

--- src/ca/mcgill/ecse420/a1/Question3.java
+++ src/ca/mcgill/ecse420/a1/Question3.java
@@ -4,25 +4,7 @@ public class Question3 {
 
   public static void main(String[] args) throws Exception{
     
-    Philosopher[] philosophers = new Philosopher[5];
-    Object[] chopsticks = new Object[philosophers.length];
-
-    for (int i = 0; i < chopsticks.length; i++) {
-        chopsticks[i] = new Object();
-    }
-
-    for (int i = 0; i < philosophers.length; i++) {
-        Object leftFork = chopsticks[i];
-        Object rightFork = chopsticks[(i + 1) % chopsticks.length];
-
-        philosophers[i] = new Philosopher(leftFork, rightFork);
-         
-        Thread t 
-          = new Thread(philosophers[i], "Philosopher " + (i + 1));
-        t.start();
-    }
-    
-    public static class Philosopher implements Runnable {
+    class Philosopher implements Runnable {
       
       // The forks on either side of this Philosopher 
       private Object Chopstick_L;
@@ -34,8 +16,7 @@ public class Question3 {
       }
       
       private void doSomething(String action) throws InterruptedException {
-        System.out.println(
-          Thread.currentThread().getName() + " " + action);
+        System.out.println(Thread.currentThread().getName() + " " + action);
         Thread.sleep(((int) (Math.random() * 100)));
       }
    
@@ -66,6 +47,25 @@ public class Question3 {
       }
    
     }
+    
+    Philosopher[] philosophers = new Philosopher[5];
+    Object[] chopsticks = new Object[philosophers.length];
+
+    for (int i = 0; i < chopsticks.length; i++) {
+        chopsticks[i] = new Object();
+    }
+
+    for (int i = 0; i < philosophers.length; i++) {
+        Object leftFork = chopsticks[i];
+        Object rightFork = chopsticks[(i + 1) % chopsticks.length];
+
+        philosophers[i] = new Philosopher(leftFork, rightFork);
+         
+        Thread t 
+          = new Thread(philosophers[i], "Philosopher " + (i + 1));
+        t.start();
+    }
+  
 
   }
 
