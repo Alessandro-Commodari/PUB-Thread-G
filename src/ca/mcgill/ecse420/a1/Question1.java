660eac7f60c8f786c9296a36ff53ef30971f6a69
Alessandro-Commodari <commodari.a@gmail.com>
2017-10-12 15:23:00 -0400

    Q1.2 doneish

--- src/ca/mcgill/ecse420/a1/Question1.java
+++ src/ca/mcgill/ecse420/a1/Question1.java
@@ -6,12 +6,13 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class Question1 {
+  // static variables used by the parallel matrix multiplication (shared resources)
+  public static double[][] parMatrixA = createRandomMatrix(3, 3);
+  public static double[][] parMatrixB = createRandomMatrix(3, 3);
+  public static double[][] parMatrixResult = new double[3][3];
+  public static int numOfThreads = 4;
 
-  public static double[][] parMatrixA = createRandomMatrix(4, 4);
-  public static double[][] parMatrixB = createRandomMatrix(4, 4);
-  public static double[][] parMatrixResult = new double[4][4];
-  public static int numOfThreads = 8;
-
+  // this class extends Runnable and will be used to run the parallel multiplication
   static class MatrixMultiplication implements Runnable {
     private int start;
     private int end;
@@ -33,18 +34,22 @@ public class Question1 {
     }
   }
 
+  // this method takes in an int for row size and one for column and fills a 2D double array of that
+  // size with random values.
   public static double[][] createRandomMatrix(int rowSize, int colSize) {
     double[][] matrix = new double[rowSize][colSize];
 
     Random randNum = new Random();
     for (int i = 0; i < rowSize; i++) {
       for (int j = 0; j < colSize; j++) {
+        // make sure the numbers are type double and between 1 and 9 without decimals for simplicity
         matrix[i][j] = Math.ceil(randNum.nextDouble() * 9);
       }
     }
     return matrix;
   }
 
+  // this method takes in two matrices and performs matrix multiplication on them sequentally
   public static double[][] sequentialMultiplyMatrix(double[][] a, double[][] b) {
     int rowA = a.length;
     int colA = a[0].length;
@@ -53,6 +58,7 @@ public class Question1 {
 
     double[][] matrix = new double[rowA][colB];
 
+    // throw an error if the two matrices cannot be multiplied with each other
     if (colA != rowB) {
       throw new IllegalArgumentException(
           "ERROR: Matrix A's column length must be equal to matrix B's row length.");
@@ -69,14 +75,14 @@ public class Question1 {
     return matrix;
   }
 
+  // this method takes in two matrices and performs matrix multiplication on them using
   public static double[][] parallelMultiplyMatrix(double[][] a, double[][] b) {
     int rowA = a.length;
     int colA = a[0].length;
     int rowB = b.length;
     int colB = b[0].length;
 
-    double[][] matrix = new double[rowA][colB];
-
+    // throw an error if the two matrices cannot be multiplied with each other
     if (colA != rowB) {
       throw new IllegalArgumentException(
           "ERROR: Matrix A's column length must be equal to matrix B's row length.");
@@ -84,11 +90,11 @@ public class Question1 {
 
     ExecutorService executor = Executors.newFixedThreadPool(numOfThreads);
     for (int i = 0; i < numOfThreads; i++) {
-      if (4 % numOfThreads != 0 && i == numOfThreads - 1) {
-        executor.execute(new MatrixMultiplication(i * (4 / numOfThreads), 4));
+      if (rowB % numOfThreads != 0 && i == numOfThreads - 1) {
+        executor.execute(new MatrixMultiplication(i * (rowB / numOfThreads), rowB));
       } else {
         executor.execute(
-            new MatrixMultiplication(i * (4 / numOfThreads), (i + 1) * (4 / numOfThreads)));
+            new MatrixMultiplication(i * (rowB / numOfThreads), (i + 1) * (rowB / numOfThreads)));
       }
     }
     executor.shutdown();
@@ -98,9 +104,10 @@ public class Question1 {
 
     }
 
-    return matrix;
+    return parMatrixResult;
   }
 
+  // this method goes throw the given matrix and outputs it
   public static void printMatrix(double[][] matrix) {
     for (int i = 0; i < matrix.length; i++) {
       System.out.print("[");
@@ -116,20 +123,20 @@ public class Question1 {
     double[][] matrixA = createRandomMatrix(3, 3);
     double[][] matrixB = createRandomMatrix(3, 3);
 
-    System.out.println("Matrix A: ");
+    System.out.println("Sequential Matrix A: ");
     printMatrix(matrixA);
-    System.out.println("Matrix B: ");
+    System.out.println("Sequential Matrix B: ");
     printMatrix(matrixB);
     System.out.println("======");
-    System.out.println("Result: ");
+    System.out.println("Sequential Result: ");
     printMatrix(sequentialMultiplyMatrix(matrixA, matrixB));
     System.out.println("\n/****************/\n");
-    System.out.println("Matrix A: ");
+    System.out.println("Parallel Matrix A: ");
     printMatrix(parMatrixA);
-    System.out.println("Matrix B: ");
+    System.out.println("Parallel Matrix B: ");
     printMatrix(parMatrixB);
     System.out.println("======");
-    System.out.println("Result: ");
+    System.out.println("Parallel Result: ");
     printMatrix(parallelMultiplyMatrix(parMatrixA, parMatrixB));
   }
 }
