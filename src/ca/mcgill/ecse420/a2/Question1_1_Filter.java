42e496461ac358d186a9b725fdfd6fef59e02dad
Alessandro-Commodari <commodari.a@gmail.com>
2017-11-08 16:04:04 -0500

    stuff

---
+++ src/ca/mcgill/ecse420/a2/Question1_1_Filter.java
@@ -0,0 +1,72 @@
+package ca.mcgill.ecse420.a2;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+public class Question1_1_Filter implements Lock {
+  int[] level;
+  int[] victim;
+
+  public Question1_1_Filter(int n) {
+    level = new int[n];
+    victim = new int[n];
+    for (int i = 0; i < n; i++) {
+      level[i] = 0;
+    }
+  }
+
+  public void lock() {
+    int me = (int) Thread.currentThread().getId();
+    int n = level.length;
+    for (int i = 1; i < n; i++) {
+      level[me] = i;
+      victim[i] = me;
+      boolean conflicts = true;
+      while (conflicts) {
+        conflicts = false;
+        for (int k = 1; k < n; k++) {
+          if (k != me && level[k] >= i && victim[i] == me) {
+            conflicts = true;
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  public void unlock() {
+    int me = (int) Thread.currentThread().getId();
+    level[me] = 0;
+  }
+
+
+
+  /* not needed for this algorithm */
+
+
+
+  @Override
+  public void lockInterruptibly() throws InterruptedException {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public Condition newCondition() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public boolean tryLock() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+    // TODO Auto-generated method stub
+    return false;
+  }
+}
---
+++ src/ca/mcgill/ecse420/a2/Question1_3_Bakery.java
@@ -0,0 +1,74 @@
+package ca.mcgill.ecse420.a2;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+public class Question1_3_Bakery implements Lock {
+
+  boolean[] flag;
+  int[] label;
+
+  public Question1_3_Bakery(int n) {
+    flag = new boolean[n];
+    label = new int[n];
+    for (int i = 0; i < n; i++) {
+      flag[i] = false;
+      label[i] = 0;
+    }
+  }
+
+  @Override
+  public void lock() {
+    int i = (int) Thread.currentThread().getId();
+    int n = label.length;
+    flag[i] = true;
+    int maxVal = -1;
+    for (int j = 1; j < n; j++) {
+      if (label[j] > maxVal) {
+        maxVal = label[j];
+      }
+    }
+    label[i] = maxVal + 1;
+    for (int k = 0; k < n; k++) {
+      while ((k != i) && (flag[k] && (label[k]) < (label[i]))
+          || ((label[k] == label[i]) && k < i)) {
+      } ;
+    }
+  }
+
+  @Override
+  public void unlock() {
+    flag[(int) Thread.currentThread().getId()] = false;
+  }
+
+
+
+  /* not needed for this algorithm */
+
+
+
+  @Override
+  public void lockInterruptibly() throws InterruptedException {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public boolean tryLock() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public Condition newCondition() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+}
