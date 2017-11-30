12beb326fe8d8accb142fe026774489490f1d27d
VittorioG <vittorio.mcgill@gmail.com>
2017-11-30 14:58:30 -0500

    2.1 final methods

--- src/ca/mcgill/ecse420/a3/FineGrainedList.java
+++ src/ca/mcgill/ecse420/a3/FineGrainedList.java
@@ -32,4 +32,60 @@ public class FineGrainedList<T> {
     }
   }
 
+  public boolean add(int item) {
+    head.lock();
+    Node pred = head;
+    try {
+      Node curr = pred.currNext();
+      curr.lock();
+      try {
+        while (curr.key() < item) {
+          pred.unlock();
+          pred = curr;
+          curr = curr.currNext();
+          curr.lock();
+        }
+        if (curr.key() == item) {
+          return false;
+        }
+        Node newNode = new Node(item);
+        newNode.newNext(curr);
+        pred.next = newNode;
+        return true;
+      } finally {
+        curr.unlock();
+      }
+    } finally {
+      pred.unlock();
+    }
+  }
+
+  public boolean remove(int item) {
+    Node pred = null;
+    Node curr = null;
+    head.lock();
+    try {
+      pred = head;
+      curr = pred.currNext();
+      curr.lock();
+      try {
+        while (curr.key() < item) {
+          pred.unlock();
+          pred = curr;
+          curr = curr.currNext();
+          curr.lock();
+        }
+        if (curr.key() == item) {
+          pred.newNext(curr.currNext());
+          return true;
+        }
+        return false;
+      } finally {
+        curr.unlock();
+      }
+    } finally {
+      pred.unlock();
+    }
+  }
+
 }
